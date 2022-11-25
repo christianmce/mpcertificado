fc793909a964d6b8d83ada9822cf48a2170508f9
ChristianMCE <cmce@unach.mx>
2022-11-25 08:12:20 -0600

    Add files via upload

---
+++ codetienda/detail.php
@@ -0,0 +1,228 @@
+<?php
+
+// SDK de Mercado Pago
+require __DIR__ . '/vendor/autoload.php';
+
+date_default_timezone_set('America/Mexico_City');
+
+// $domain = 'http://mercadopago.test/';
+$domain = 'http://christianmce.net/tiendacmce/';
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('APP_USR-3934048546363976-090514-6089f69a50dc7ded530bf06fda1cc0c4-818322374');
+MercadoPago\SDK::setIntegratorId('APP_USR-b28c0399-3ba8-4747-b076-46ce3220f8a1');
+
+$payer               = new MercadoPago\Payer();
+$payer->name         = 'Lalo';
+$payer->surname      = 'Landa';
+$payer->email        = 'test_user_81131286@testuser.com';
+$payer->phone        = [
+    'area_code' => '011',
+    'number'    => '2222-3333'
+];
+$payer->identification = [
+    'type'   => 'DNI',
+    'number' => '3934048546363976'
+];
+
+$payer->address = [
+    'street_name'   => 'Solidaridad Las Vegas',
+    'street_number' => '1',
+    'zip_code'      => '30798'
+];
+
+// Crea un ítem en la preferencia
+$item              = new MercadoPago\Item();
+$item->id          = '1234';
+$item->title       = $_POST['title'];
+$item->description = 'Dispositivo móvil de Tienda e-commerce';
+$item->picture_url = $domain . str_replace('./', '', $_POST['img']);
+$item->quantity    = 1;
+$item->currency_id = 'MXN';
+$item->unit_price = $_POST['price'];
+
+// Crea un objeto de preferencia
+$preference                  = new MercadoPago\Preference();
+$preference->payment_methods = [
+    'excluded_payment_methods' => [
+        ['id' => 'amex']
+    ],
+    'excluded_payment_types' => [
+        ['id' => 'atm']
+    ],
+    'installments' => 6
+];
+$preference->external_reference = 'christianmce@gmail.com';
+$preference->back_urls          = [
+    'success' => $domain . 'response.php?mp_status=success',
+    'failure' => $domain . 'response.php?mp_status=failure',
+    'pending' => $domain . 'response.php?mp_status=pending'
+];
+$preference->auto_return = 'approved';
+$preference->notification_url = $domain.'notification.php?source_news=webhooks';
+$preference->items = [$item];
+$preference->payer = $payer;
+$preference->save();
+
+?>
+
+<!DOCTYPE html>
+<html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=1024">
+    <title>Tienda e-commerce</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="format-detection" content="telephone=no">
+
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+
+    <script src="https://www.mercadopago.com/v2/security.js" view="item"></script>
+
+    <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
+
+    <link rel="stylesheet" href="./assets/category.css" media="screen, print">
+
+    <link rel="stylesheet" href="./assets/merch-tools.css" media="screen, print">
+
+    <link rel="stylesheet" href="./assets/fonts" media="">
+    <style>
+        .as-filter-button-text {
+            font-size: 26px;
+            font-weight: 700;
+            color: #333;
+        }
+        .row.as-fixed-nav {
+            border-bottom: 1px solid #ddd;
+        }
+        .as-producttile-tilehero.with-paddlenav.with-paddlenav-onhover {
+            height: 330px;
+        }
+        .as-footnotes {
+            background: #333;
+            color: #fff;
+            padding: 16px 40px;
+        }
+    </style>
+    <style type="text/css"> @keyframes loading-rotate { 100% { transform: rotate(360deg); } } @keyframes loading-dash { 0% { stroke-dasharray: 1, 200; stroke-dashoffset: 0; } 50% { stroke-dasharray: 100, 200; stroke-dashoffset: -20px; } 100% { stroke-dasharray: 89, 200; stroke-dashoffset: -124px; } } @keyframes loading-fade-in { from { opacity: 0; } to { opacity: 1; } } .mp-spinner { position: absolute; top: 100px; left: 50%; font-size: 70px; margin-left: -35px; animation: loading-rotate 2.5s linear infinite; transform-origin: center center; width: 1em; height: 1em; } .mp-spinner-path { stroke-dasharray: 1, 200; stroke-dashoffset: 0; animation: loading-dash 1.5s ease-in-out infinite; stroke-linecap: round; stroke-width: 2px; stroke: #009ee3; } </style><style type="text/css"> .mercadopago-button { padding: 0 1.7142857142857142em; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; } </style><style type="text/css"> @keyframes loading-rotate { 100% { transform: rotate(360deg); } } @keyframes loading-dash { 0% { stroke-dasharray: 1, 200; stroke-dashoffset: 0; } 50% { stroke-dasharray: 100, 200; stroke-dashoffset: -20px; } 100% { stroke-dasharray: 89, 200; stroke-dashoffset: -124px; } } @keyframes loading-fade-in { from { opacity: 0; } to { opacity: 1; } } .mp-spinner { position: absolute; top: 100px; left: 50%; font-size: 70px; margin-left: -35px; animation: loading-rotate 2.5s linear infinite; transform-origin: center center; width: 1em; height: 1em; } .mp-spinner-path { stroke-dasharray: 1, 200; stroke-dashoffset: 0; animation: loading-dash 1.5s ease-in-out infinite; stroke-linecap: round; stroke-width: 2px; stroke: #009ee3; } </style><style type="text/css"> .mercadopago-button { padding: 0 1.7142857142857142em; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; } </style><style type="text/css"> @keyframes loading-rotate { 100% { transform: rotate(360deg); } } @keyframes loading-dash { 0% { stroke-dasharray: 1, 200; stroke-dashoffset: 0; } 50% { stroke-dasharray: 100, 200; stroke-dashoffset: -20px; } 100% { stroke-dasharray: 89, 200; stroke-dashoffset: -124px; } } @keyframes loading-fade-in { from { opacity: 0; } to { opacity: 1; } } .mp-spinner { position: absolute; top: 100px; left: 50%; font-size: 70px; margin-left: -35px; animation: loading-rotate 2.5s linear infinite; transform-origin: center center; width: 1em; height: 1em; } .mp-spinner-path { stroke-dasharray: 1, 200; stroke-dashoffset: 0; animation: loading-dash 1.5s ease-in-out infinite; stroke-linecap: round; stroke-width: 2px; stroke: #009ee3; } </style><style type="text/css"> .mercadopago-button { padding: 0 1.7142857142857142em; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; } </style><style type="text/css"> @keyframes loading-rotate { 100% { transform: rotate(360deg); } } @keyframes loading-dash { 0% { stroke-dasharray: 1, 200; stroke-dashoffset: 0; } 50% { stroke-dasharray: 100, 200; stroke-dashoffset: -20px; } 100% { stroke-dasharray: 89, 200; stroke-dashoffset: -124px; } } @keyframes loading-fade-in { from { opacity: 0; } to { opacity: 1; } } .mp-spinner { position: absolute; top: 100px; left: 50%; font-size: 70px; margin-left: -35px; animation: loading-rotate 2.5s linear infinite; transform-origin: center center; width: 1em; height: 1em; } .mp-spinner-path { stroke-dasharray: 1, 200; stroke-dashoffset: 0; animation: loading-dash 1.5s ease-in-out infinite; stroke-linecap: round; stroke-width: 2px; stroke: #009ee3; } </style><style type="text/css"> .mercadopago-button { padding: 0 1.7142857142857142em; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; } </style><style type="text/css"> @keyframes loading-rotate { 100% { transform: rotate(360deg); } } @keyframes loading-dash { 0% { stroke-dasharray: 1, 200; stroke-dashoffset: 0; } 50% { stroke-dasharray: 100, 200; stroke-dashoffset: -20px; } 100% { stroke-dasharray: 89, 200; stroke-dashoffset: -124px; } } @keyframes loading-fade-in { from { opacity: 0; } to { opacity: 1; } } .mp-spinner { position: absolute; top: 100px; left: 50%; font-size: 70px; margin-left: -35px; animation: loading-rotate 2.5s linear infinite; transform-origin: center center; width: 1em; height: 1em; } .mp-spinner-path { stroke-dasharray: 1, 200; stroke-dashoffset: 0; animation: loading-dash 1.5s ease-in-out infinite; stroke-linecap: round; stroke-width: 2px; stroke: #009ee3; } </style><style type="text/css"> .mercadopago-button { padding: 0 1.7142857142857142em; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; } </style><style type="text/css"> @keyframes loading-rotate { 100% { transform: rotate(360deg); } } @keyframes loading-dash { 0% { stroke-dasharray: 1, 200; stroke-dashoffset: 0; } 50% { stroke-dasharray: 100, 200; stroke-dashoffset: -20px; } 100% { stroke-dasharray: 89, 200; stroke-dashoffset: -124px; } } @keyframes loading-fade-in { from { opacity: 0; } to { opacity: 1; } } .mp-spinner { position: absolute; top: 100px; left: 50%; font-size: 70px; margin-left: -35px; animation: loading-rotate 2.5s linear infinite; transform-origin: center center; width: 1em; height: 1em; } .mp-spinner-path { stroke-dasharray: 1, 200; stroke-dashoffset: 0; animation: loading-dash 1.5s ease-in-out infinite; stroke-linecap: round; stroke-width: 2px; stroke: #009ee3; } </style><style type="text/css"> .mercadopago-button { padding: 0 1.7142857142857142em; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; } </style></head>
+
+
+
+<body class="as-theme-light-heroimage">
+
+    <div class="stack">
+        
+        <div class="as-search-wrapper" role="main">
+            <div class="as-navtuck-wrapper">
+                <div class="as-l-fullwidth  as-navtuck" data-events="event52">
+                    <div>
+                        <div class="pd-billboard pd-category-header">
+                            <div class="pd-l-plate-scale">
+                                <div class="pd-billboard-background">
+                                    <img src="./assets/music-audio-alp-201709" alt="" width="1440" height="320" data-scale-params-2="wid=2880&amp;hei=640&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1503948581306" class="pd-billboard-hero ir">
+                                </div>
+                                <div class="pd-billboard-info">
+                                    <h1 class="pd-billboard-header pd-util-compact-small-18">Tienda e-Commerce ChristianMCE</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="as-search-results as-filter-open as-category-landing as-desktop" id="as-search-results">
+
+                <div id="accessories-tab" class="as-accessories-details">
+                    <div class="as-accessories" id="as-accessories">
+                        <div class="as-accessories-header">
+                            <div class="as-search-results-count">
+                                <span class="as-search-results-value"></span>
+                            </div>
+                        </div>
+                        <div class="as-searchnav-placeholder" style="height: 77px;">
+                            <div class="row as-search-navbar" id="as-search-navbar" style="width: auto;">
+                                <div class="as-accessories-filter-tile column large-6 small-3">
+
+                                    <button class="as-filter-button" aria-expanded="true" aria-controls="as-search-filters" type="button">
+                                        <h2 class=" as-filter-button-text">
+                                            Smartphones
+                                        </h2>
+                                    </button>
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="as-accessories-results  as-search-desktop">
+                            <div class="width:60%">
+                                <div class="as-producttile-tilehero with-paddlenav " style="float:left;">
+                                    <div class="as-dummy-container as-dummy-img">
+
+                                        <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " style="max-width: 70%;max-height: 70%;"alt="" width="445" height="445">
+                                    </div>
+                                    <div class="images mini-gallery gal5 ">
+                                    
+
+                                        <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
+                                            <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
+                                                <div class="as-tilegallery-element as-image-selected">
+                                                    <div class=""></div>
+                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
+                                                </div>
+                                                
+                                            </div>
+
+                                            
+                                        </div>
+
+                                        
+
+                                    </div>
+
+                                </div>
+                                <div class="as-producttile-info" style="float:left;min-height: 168px;">
+                                    <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
+                                        <div class="as-producttile-title">
+                                            <h3 class="as-producttile-name">
+                                                <p class="as-producttile-tilelink">
+                                                    <span data-ase-truncate="2"><b></b><?php echo $_POST['title'] ?></b></span>
+                                                </p>
+
+                                            </h3>
+                                        </div>
+                                        <h3 class="as-producttile-name">
+                                            <?php echo "$ ".money_format('%i', $_POST['price'])." pesos MX" ?>
+                                        </h3>
+                                        <h3 class="as-producttile-name">
+                                            <?php echo "Cantidad: ". $_POST['unit'] ?>
+                                        </h3>
+                                    </div>
+                                    <br><br>
+                                    <div class="mt-2">
+                                        <a href="<?= $preference->init_point ?>" class="mercadopago-button" style="height:80px;">PAGAR LA COMPRA</a>
+                                    </div>
+                                    <br><br>
+                                    <a href="./" class="btn-back"><b><< Regresar al Home</b></a>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br><br><br>
+        <div role="alert" class="as-loader-text ally" aria-live="assertive"></div>
+        <div class="as-footnotes">
+            <div class="as-footnotes-content">
+                <div class="as-footnotes-sosumi">
+                    2021 - @Todos los derechos reservados Tienda Online - Christian Mauricio Castillo Estrada
+                </div>
+            </div>
+        </div>
+
+</div>
+
+</body></html>
